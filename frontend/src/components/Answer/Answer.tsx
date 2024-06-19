7d22d1751c14149d68d65bad8a80678cd7f35aa0
Abigail Hartman <Abigail.Hahn@microsoft.com>
2024-06-19 12:47:34 -0700

    Remove docker login step from the build workflow (#943)

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -15,7 +15,6 @@ import { AppStateContext } from '../../state/AppProvider'
 import { parseAnswer } from './AnswerParser'
 
 import styles from './Answer.module.css'
-import config from '../../../citation_config.json';
 interface Props {
   answer: AskResponse
   onCitationClicked: (citedDocument: Citation) => void
@@ -66,7 +65,7 @@ export const Answer = ({ answer, onCitationClicked, onExectResultClicked }: Prop
     setFeedbackState(currentFeedbackState)
   }, [appStateContext?.state.feedbackState, feedbackState, answer.message_id])
 
-  const createCitationFilepath = (citation: Citation, index: number, truncate: boolean = false) => {
+   const createCitationFilepath = (citation: Citation, index: number, truncate: boolean = false) => {
     let citationFilename = ''
 
     if (citation.filepath) {
@@ -74,40 +73,17 @@ export const Answer = ({ answer, onCitationClicked, onExectResultClicked }: Prop
       if (truncate && citation.filepath.length > filePathTruncationLimit) {
         const citationLength = citation.filepath.length
         citationFilename = `${citation.filepath.substring(0, 20)}...${citation.filepath.substring(citationLength - 20)} - Part ${part_i}`
-        citationFilename = checkConfig(citation, citationFilename);         
       } else {
         citationFilename = `${citation.filepath} - Part ${part_i}`
-        citationFilename = checkConfig(citation, citationFilename); 
-        
       }
     } else if (citation.filepath && citation.reindex_id) {
       citationFilename = `${citation.filepath} - Part ${citation.reindex_id}`
-      citationFilename = checkConfig(citation, citationFilename); 
-      
     } else {
       citationFilename = `Citation ${index}`
     }
-    citation.url = citationFilename;
     return citationFilename
   }
-
-  const checkConfig = (citation: Citation, citationFilename: string) => {
-    let filepath = "";
-    console.log(JSON.stringify(config));
-    config.forEach(obj => {
-        Object.entries(obj).forEach(([key, value]) => {
-            console.log(`${key} - ${value}`);
-            if(citation.filepath != null && citation.filepath.includes(obj.filename)){
-                console.log(obj.sharepointUrl);
-                filepath = obj.sharepointUrl;                                        
-            }                                
-        });            
-    });
-    if (filepath == "") 
-      filepath = citationFilename;
-    return filepath;        
-  }
-
+ 
   const onLikeResponseClicked = async () => {
     if (answer.message_id == undefined) return
 
