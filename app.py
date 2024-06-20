58754d1ec9ee8b6c51873ec83274f12cb92bdde1
Ian Cassidy <ianlcassidy@gmail.com>
2024-06-20 12:00:00 -0400

    Bug: fix generate title function (#933)

--- app.py
+++ app.py
@@ -1062,7 +1062,7 @@ async def generate_title(conversation_messages) -> str:
     messages.append({"role": "user", "content": title_prompt})
 
     try:
-        azure_openai_client = await init_openai_client()
+        azure_openai_client = init_openai_client()
         response = await azure_openai_client.chat.completions.create(
             model=app_settings.azure_openai.model, messages=messages, temperature=1, max_tokens=64
         )
